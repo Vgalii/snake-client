6ba492c02f35c9cbd83f011596fab98b72275283
Vgalii <veronicaggalii@gmail.com>
2021-12-01 15:22:57 -0500

    Created connect and wtiteName functions

--- client.js
+++ client.js
@@ -1,20 +1,26 @@
 const net = require("net");
 // establishes a connection with the game server
-const connect = function () {
+const connect = function() {
   const conn = net.createConnection({
     host: '165.227.47.243',
     port: 50541
   });
-   // interpret incoming data as text
-   conn.setEncoding("utf8");
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
 
-   return conn;
- };
- const conn = connect();
+  conn.on("data", data => {
+    console.log(data);
+  });
+  conn.on("connect", () => {
+    console.log("Connection established...");
+    conn.write("Name: VMG");
+  });
 
- conn.on("data", data => {
-  console.log(data);
-})
+  return conn;
+ 
+ 
+
+};
 
 module.exports = connect;
 
